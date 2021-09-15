5c968b733ba66f26c49cdf263701349cc4d44692
stibipet <stibipet@fel.cvut.cz>
2021-09-15 09:37:28 +0200

    publish desired pose

--- launch/control_interface.py
+++ launch/control_interface.py
@@ -41,6 +41,7 @@ def generate_launch_description():
                 remappings=[
                     ("~/vehicle_command_out", "/" + DRONE_DEVICE_ID + "/VehicleCommand_PubSubTopic"),
                     ("~/local_odom_out", "~/local_odom"),
+                    ("~/desired_pose_out", "~/desired_pose"),
                     ("~/diagnostics_out", "~/diagnostics"),
                     ("~/debug_markers_out", "~/debug/waypoint_markers"),
 
